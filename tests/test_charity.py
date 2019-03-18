7150bb0762705574e77e27afe4f82fdcb665ee7d
David Kane <d.r.kane@gmail.com>
2019-03-18 15:31:15 +0000

    get charity working

---
+++ tests/test_charity.py
@@ -0,0 +1,22 @@
+from starlette.testclient import TestClient
+
+from findthatcharity.app import app
+
+def test_charity_missing():
+    client = TestClient(app)
+    response = client.get('/charity/ABCDASD')
+    assert response.status_code == 404
+    assert "not found" in response.text.lower()
+
+def test_charity_json():
+    client = TestClient(app)
+    response = client.get('/charity/225922.json')
+    assert response.status_code == 200
+    result = response.json()
+    assert result["known_as"] == "The National Council for Voluntary Organisations"
+
+def test_charity():
+    client = TestClient(app)
+    response = client.get('/charity/225922.html')
+    assert response.status_code == 200
+    assert "National Council for Voluntary Organisations".lower() in response.text.lower()
