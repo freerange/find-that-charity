f7d45a3c381e57bf297900835c6e1833ef362be0
David Kane <d.r.kane@gmail.com>
2019-07-04 21:32:20 +0100

    dokku fix

--- findthatcharity/templates.py
+++ findthatcharity/templates.py
@@ -19,5 +19,5 @@ vals = value_counts()
 templates.env.filters["list_to_string"] = list_to_string
 templates.env.globals["sources"] = fetch_all_sources()
 templates.env.globals["org_types"] = sort_out_orgtypes(vals.get("group_by_type", {}).get("buckets",[]))
-templates.env.globals["sources_count"] = vals["group_by_source"]["buckets"]
+templates.env.globals["sources_count"] = vals.get("group_by_source", {}).get("buckets", [])
 templates.env.globals["key_types"] = settings.KEY_TYPES
