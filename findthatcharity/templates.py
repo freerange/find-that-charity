79ee1dc4cc0c62fde176b5d68d089c3533cffb9c
David Kane <david@dkane.net>
2020-02-11 14:22:06 +0000

    better orgid prioritisation - issue #57

--- findthatcharity/templates.py
+++ findthatcharity/templates.py
@@ -10,11 +10,19 @@ from . import settings
 def regex_search(s, regex):
     return re.search(regex, s) is not None
 
+def prioritise_orgids(orgids):
+    if len(orgids)==1:
+        return orgids
+    prefixes = ["-".join(o.split("-")[0:2]) for o in orgids]
+    order = [settings.PRIORITIES.index(p) if p in settings.PRIORITIES else len(settings.PRIORITIES) + 1 for p in prefixes]
+    return [x for _,x in sorted(zip(order,orgids))]
+
 templates = Jinja2Templates(directory='templates')
 
 templates.env.filters["list_to_string"] = list_to_string
 templates.env.filters["regex_search"] = regex_search
 templates.env.filters["slugify"] = slugify
+templates.env.filters["prioritise"] = prioritise_orgids
 templates.env.globals["sources"] = fetch_all_sources()
 templates.env.globals["org_types"] = ORGTYPES
 templates.env.globals["sources_count"] = SOURCES
