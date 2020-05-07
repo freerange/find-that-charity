4202d6e22b832867023bfa98e7841ad915ca06b5
David Kane <david@dkane.net>
2020-05-07 13:34:25 +0100

    don't fail if no start time

--- findthatcharity/apps/admin.py
+++ findthatcharity/apps/admin.py
@@ -23,7 +23,7 @@ async def get_scrapes(request):
         {
             **s,
             "stats": json.loads(s['stats']),
-            "start_date": s["start_time"].date(),
+            "start_date": s["start_time"].date() if s.get("start_time") else None,
         } for s in scrapes
     
     ]
